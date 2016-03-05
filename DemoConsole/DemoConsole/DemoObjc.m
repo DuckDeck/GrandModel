8e8593e5a29d7052364dffbf090463e7c0667028
Stan <StanHu@HuStans-MBP.lan>
2016-03-05 23:40:14 +0800

    Reset Grand Model

---
+++ DemoConsole/DemoConsole/DemoObjc.m
@@ -0,0 +1,13 @@
+//
+//  DemoObjc.m
+//  DemoConsole
+//
+//  Created by HuStan on 3/5/16.
+//  Copyright © 2016 StanHu. All rights reserved.
+//
+
+#import "DemoObjc.h"
+#import "DemoConsole-Swift.h"
+@implementation DemoObjc
+
+@end
