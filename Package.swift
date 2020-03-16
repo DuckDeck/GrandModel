2b2d1438c79b6252254adc1cfbf25ac2bf9e3bd2
“stanhu” <3421902@qq.com>
2020-03-16 17:51:28 +0800

    process code

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "GrandModel",
-            path: "Source"),
+            path: "Sources"),
         .testTarget(
             name: "GrandModelTests",
             dependencies: ["GrandModel"]),
